55a75c9491268914b814b43d50b92718e2acaf64
ubuntu2310fake <152691685+ubuntu2310fake@users.noreply.github.com>
2026-03-01 14:51:36 +0700

    Add files via upload

---
+++ includes/db_config.php
@@ -0,0 +1,11 @@
+<?php
+// includes/db_config.php
+// KHÁCH HÀNG TỰ CHỈNH SỬA FILE NÀY
+
+$db_host = '127.0.0.1';
+$db_name = 'quan_ly_ne_nep';
+$db_user = 'root';
+$db_pass = 'MẬT KHẨU';
+$db_charset = 'utf8mb4';
+
+?>
